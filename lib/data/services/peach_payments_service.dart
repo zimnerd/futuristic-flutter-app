e086b1ec2e9606eb176e777cc823c1f7dd820d00
Eddy Ganz <eddienyagano@gmail.com>
2025-09-13 23:22:55 +0200

    feat: Add logger import for enhanced logging capabilities in PeachPayments service

--- lib/data/services/peach_payments_service.dart
+++ lib/data/services/peach_payments_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import '../../core/utils/logger.dart';
 
 /// PeachPayments API integration service
