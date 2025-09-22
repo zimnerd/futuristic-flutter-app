9955a5db998aa22ee539f83233355f238ce67b9e
Eddy Ganz <eddienyagano@gmail.com>
2025-09-22 20:54:04 +0200

    feat: Update app icons and colors, modify app name, and adjust environment settings
    - Updated Android and iOS app launcher icons across various resolutions.
    - Added a new colors.xml file for the launcher background color.
    - Modified the iOS project settings to specify AppIcon for asset catalog.
    - Updated the Info.plist to change the app display name from "Frontend" to "PulseLink".
    - Changed the AppConfig to use the production environment.
    - Cleaned up pubspec.lock by removing unused dependencies and updated dev dependencies.
    - Configured flutter_launcher_icons for both Android and iOS with a specified adaptive icon background and foreground.

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -7,7 +7,7 @@ class AppConfig {
   // ================================
   // Change this manually to switch between dev/prod
   static const bool _useProductionEnvironment =
-      false; // Set to true for production
+      true; // Set to true for production
 
   // Environment flags based on manual setting
   static const bool isDevelopment = !_useProductionEnvironment;
